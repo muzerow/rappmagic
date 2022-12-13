7fdd1506c27a29669cfe1f3c674279ca06ecc6d8
Kirill Mukhin <muzerow6@gmail.com>
2022-12-13 18:29:27 +0300

    Add am_apps.r

---
+++ R/am_apps.r
@@ -0,0 +1,25 @@
+#' Get Applications by Search Query
+#' @description Get Applications by Search Query <https://api.appmagic.rocks/v1/docs#operation/getApplications>
+#' @importFrom httr add_headers content GET
+#'
+#' @param search Search query
+#' @param limit The number of returned items
+#' @param offset Offset of returned items
+#' @param signature Authentication token
+#'
+#' @export
+
+am_apps <- function(search, limit = NULL, offset = NULL, signature) {
+  apps <- GET("https://api.appmagic.rocks/v1/applications",
+              query = list(search = search,
+                           limit = limit,
+                           offset = offset),
+              add_headers("Authorization" = signature,
+                          "Accept-Encoding" = "gzip"))
+
+  if (apps$status_code != 200) {
+    stop(paste0("Error code ", apps$status_code, ": ", content(apps)$message))
+  }
+
+  content(apps)
+}

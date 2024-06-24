32530c93e825e7a84cad3e6f60e381ce56da6ae0
Kirill Mukhin <70025661+muzerow@users.noreply.github.com>
2024-06-25 02:11:16 +0300

    Add accept parameter

--- R/am_united_apps_app_ids.r
+++ R/am_united_apps_app_ids.r
@@ -7,13 +7,13 @@
 #'
 #' @export
 
-am_united_apps_app_ids <- function(ids_array, signature) {
+am_united_apps_app_ids <- function(ids_array, signature, accept = "text/csv") {
   united_apps_info <- POST("https://api.appmagic.rocks/v1/united-applications/search-by-app-ids",
                            body = ids_array,
                            encode = "json",
                            add_headers("Authorization" = signature,
                                        "Accept-Encoding" = "gzip",
-                                       "Accept" = "text/csv"))
+                                       "Accept" = accept))
 
   if (united_apps_info$status_code != 200) {
     stop(paste0("Error code ", united_apps_info$status_code, ": ", content(united_apps_info)$message))
